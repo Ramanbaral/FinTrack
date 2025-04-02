912d0eb09b5e200c891fda59a74f24232fede452
raman baral <ramanisstudent@gmail.com>
2025-04-02 23:19:30 +0545

    feat: added charts and budget cards in dashboard screen

---
+++ src/app/(routes)/dashboard/_components/ChartDashboard.tsx
@@ -0,0 +1,60 @@
+"use client";
+import {
+  BarChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function ChartDashboard({
+  budgetList,
+  amountSpentList,
+}: {
+  budgetList: ({
+    expenses: {
+      name: string;
+      id: number;
+      amount: string;
+      createdAt: Date;
+      updatedAt: Date;
+      budgetId: number;
+    }[];
+  } & {
+    name: string;
+    id: number;
+    amount: string;
+    icon: string | null;
+    createdBy: string;
+    createdAt: Date;
+    updatedAt: Date;
+  })[];
+  amountSpentList: number[];
+}) {
+  const data: { name: string; totalAmount: string; amountSpent: number }[] = [];
+  budgetList.forEach((budget, ind) => {
+    data.push({
+      name: budget.name,
+      totalAmount: budget.amount,
+      amountSpent: amountSpentList[ind],
+    });
+  });
+
+  return (
+    <div className="my-7 border rounded-lg p-5">
+      <h2 className="font-bold text-xl text-center">Recent Activity</h2>
+      <ResponsiveContainer width={"80%"} height={400}>
+        <BarChart data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="amountSpent" stackId="a" fill="#00b5ff" />
+          <Bar dataKey="totalAmount" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,6 +1,9 @@
 import { currentUser } from "@clerk/nextjs/server";
 import InfoCards from "./_components/InfoCards";
+import ChartDashboard from "./_components/ChartDashboard";
 import { prisma } from "@/lib/prisma";
+import BudgetCard from "./budgets/_components/BudgetCard";
+import Link from "next/link";
 
 export default async function Dashboard() {
   const user = await currentUser();
@@ -15,12 +18,21 @@ export default async function Dashboard() {
       expenses: true,
     },
   });
+  const amountSpentList: number[] = [];
+  budgets.forEach((budget) => {
+    let amountSpent = 0;
+    budget.expenses.forEach((exp) => (amountSpent += +exp.amount));
+    amountSpentList.push(amountSpent);
+  });
 
   return (
     <div className="p-5">
+      <div className="mb-2">
       <h2 className="font-semibold text-3xl">
         Hi, {user?.fullName} {"  "} 👋
       </h2>
+      <p className="text-sm text-gray-400">Here&apos;s what happenning with your money</p>
+      </div>
       {budgets.length != 0 ? (
         <InfoCards budgetList={budgets} />
       ) : (
@@ -35,6 +47,30 @@ export default async function Dashboard() {
           })}
         </div>
       )}
+
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="col-span-2">
+          <ChartDashboard budgetList={budgets} amountSpentList={amountSpentList} />
+        </div>
+        <div className="h-[56.9vh] overflow-scroll no-scrollbar my-7 ml-4">
+          <h2 className="font-bold text-xl mb-2">Latest Budgets</h2>
+          {budgets.map((budget, ind) => {
+            return (
+              <div className="mb-2" key={budget.id}>
+                <Link href={`/dashboard/expenses/${budget.id}`}>
+                  <BudgetCard
+                    name={budget.name}
+                    amount={budget.amount}
+                    icon={budget.icon}
+                    amountSpent={amountSpentList[ind]}
+                    expenseCount={budget.expenses.length}
+                  />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
